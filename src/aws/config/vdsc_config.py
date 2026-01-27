369e6aec8bb1a393b7d56066cc7d20618106309c
titoparizotto <titoparizotto@gmail.com>
2026-01-27 14:40:56 -0300

    dev: adicionada pipeline com testes, sonar e sam deploy

--- src/aws/config/vdsc_config.py
+++ src/aws/config/vdsc_config.py
@@ -11,17 +11,13 @@ class VdscConfig:
 
     def _initialize(self):
         self.aws = {
-            'region': os.getenv('AWS_REGION', 'us-east-1'),
-            'account_id': os.getenv('AWS_ACCOUNT_ID', '080145351546')}
+            'region': os.getenv('AWS_REGION', 'us-east-1')
+        }
         self.s3_bucket = {
             'name': os.getenv('S3_BUCKET_NAME', 'vdsc-prd-s3-videos'),
             'dir_uploads': os.getenv('S3_BUCKET_DIR_UPLOADS', 'uploads/'),
-            'dir_finished': os.getenv('S3_BUCKET_DIR_finished', 'finished/'),
+            'dir_finished': os.getenv('S3_BUCKET_DIR_FINISHED', 'finished/'),
             'dir_processing': os.getenv('S3_BUCKET_DIR_PROCESSING', 'processing/')
-        }
-        self.sqs = {
-            'url': os.getenv('SQS_URL', 'https://sqs.us-east-1.amazonaws.com'),
-            'dlq_name': os.getenv('SQS_DLQ_NAME', 'vdsc-prd-dlq')
         }
         self.eventbus = {
             'name': os.getenv('EVENT_BUS_NAME', 'vdsc-prd-event-bus')
@@ -30,12 +26,7 @@ class VdscConfig:
             'table_name': os.getenv('DYNAMODB_TABLE_NAME', 'VideoSlice')
         }
         self.vdsc = {
-            'png_compression_level': int(os.getenv('PNG_COMPRESSION_LEVEL', '9')),
-            'zip_compression_level': int(os.getenv('ZIP_COMPRESSION_LEVEL', '5')),
-            'quality':{
-                'ultra': 1080,
-                'high': 720,
-                'medium': 480,
-                'low': 360
-            }
+            'png_compression_level': int(os.getenv('VDSC_PNG_COMPRESSION_LEVEL', '9')),
+            'zip_compression_level': int(os.getenv('VDSC_ZIP_COMPRESSION_LEVEL', '5')),
+            'quality':os.getenv('VDSC_QUALITY', "{'ultra': 1080, 'high': 720, 'medium': 480, 'low': 360}")
         }
