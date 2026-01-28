71dfa38182fd8fa9c84f5ac2dafb840dc53a78d8
titoparizotto <titoparizotto@gmail.com>
2026-01-28 10:25:49 -0300

    fix: ajustes pipeline variáveis de ambientes

--- src/aws/config/vdsc_config.py
+++ src/aws/config/vdsc_config.py
@@ -1,7 +1,7 @@
 import json
 import os
 
-quality = json.loads(os.getenv('VDSC_QUALITY', '{"ultra": 1080, "high": 720, "medium": 480, "low": 360}').replace('\"', '"'))
+quality = json.loads(os.getenv('VDSC_QUALITY', '{"ultra": 1080, "high": 720, "medium": 480, "low": 360}').replace('\\', ''))
 
 class VdscConfig:
     _instance = None
