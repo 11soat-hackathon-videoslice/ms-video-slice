f4ed982dc067414d1f788da50a4910936d362c32
Tito Parizotto <titoparizotto@gmail.com>
2026-02-08 18:25:46 -0300

    dev: implementada domínio de notificação. refatoração de testes e interfaces url e notificação

--- src/aws/config/vdsc_config.py
+++ src/aws/config/vdsc_config.py
@@ -16,7 +16,7 @@ schedule_event_rules = {
     'retry_backoff_factor':  int(os.getenv('SCHEDULE_EVENT_RETRY_BACKOFF_FACTOR', '5')),
     'retry_arn': os.getenv('SCHEDULE_EVENT_ROLE_ARN', 'arn:aws:sqs:us-east-1:080145351546:vdsc-prd-sqs-video-slice'),
     'retry_role_arn': os.getenv('SCHEDULE_EVENT_ROLE_ARN', 'arn:aws:iam::080145351546:role/vdsc-prd-schduler-role'),
-    'retry_dlq': os.getenv('SCHEDULE_EVENT_DLQ', 'arn:aws:sqs:us-east-1:080145351546:vdsc-prd-dlq')
+    'retry_dlq': os.getenv('SCHEDULE_EVENT_DLQ', 'arn:aws:sqs:us-east-1:080145351546:vdsc-prd-sqs-video-slice-dlq')
 }
 
 class VdscConfig:
